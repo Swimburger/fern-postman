5ac0ba305c1467edbff05078cac8a6a5441bb017
dsinghvi <deep@usebirch.com>
2023-01-30 15:55:22 -0800

    api key should not be a variable reference

--- src/auth.ts
+++ src/auth.ts
@@ -31,8 +31,8 @@ export function convertAuth(schemes: AuthScheme[]): PostmanRequestAuth | undefin
                         type: "string",
                     },
                 ]),
-            header: (header) =>
-                PostmanRequestAuth.apikey([
+            header: (header) => {
+                return PostmanRequestAuth.apikey([
                     {
                         key: "value",
                         value: getReferenceToVariable(getVariableForAuthHeader(header)),
@@ -40,7 +40,7 @@ export function convertAuth(schemes: AuthScheme[]): PostmanRequestAuth | undefin
                     },
                     {
                         key: "key",
-                        value: getReferenceToVariable(getVariableForAuthHeader(header)),
+                        value: header.nameV2.wireValue,
                         type: "string",
                     },
                     {
@@ -48,8 +48,10 @@ export function convertAuth(schemes: AuthScheme[]): PostmanRequestAuth | undefin
                         value: "header",
                         type: "string",
                     },
-                ]),
+                ]);
+            },
             _unknown: () => {
+                console.log("IN UNKNOWN");
                 throw new Error("Unknown auth scheme: " + scheme._type);
             },
         });
